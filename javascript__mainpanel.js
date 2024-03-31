8b4973a441a5502cc76e0288353502571af5bdb1
Jakub Bąk <kubabaczek347@gmail.com>
2024-03-31 13:45:46 +0200

    -dodałem linki do przejść

--- javascript__mainpanel.js
+++ javascript__mainpanel.js
@@ -1,12 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     let toggler = document.getElementById('toggler');
     let hiddenNavPanel = document.querySelector('.hidden_nav_panel_list');
+    let menuIcon = document.querySelector('.menu-icon img');
+
     hiddenNavPanel.style.display = 'none';
     toggler.addEventListener('change', function() {
         if (this.checked) {
             hiddenNavPanel.style.display = 'block';
+            // Zmień ikonę na zamkniętą
+            menuIcon.src = 'images/open_sidebar_menu_icon';
         } else {
             hiddenNavPanel.style.display = 'none';
+            // Zmień ikonę na otwartą
+            menuIcon.src = 'images/icons-menu.png';
         }
     });
 });
